adc481e3c650194115fc1d3d0f789d1b2631cbee
陈梓立 <wander4096@gmail.com>
2018-05-01 21:23:47 +0800

    [Init] _
    Implement extract logic, and introduce a sample extracted file.

---
+++ README.md
@@ -0,0 +1,21 @@
+# A Demo of Running RankClus Algorithm on Dblp
+
+## Synopsis
+
+This repository holds a demo of running [RankClus Algorithm](https://openproceedings.org/2009/conf/edbt/SunHZYCW09.pdf) on dblp data set.
+
+## XML Extract
+
+Require Python 3 and package lxml.
+
+The extractor is code/extractor.py.
+
+XML and DTD file are too large, so you'd better download it from http://dblp.org/xml/release/. This repository is based on [dblp-2018-04-01.xml](http://dblp.org/xml/release/dblp-2018-04-01.xml.gz) with its DTD file[dblp-2017-08-29.dtd](http://dblp.org/xml/release/dblp-2017-08-29.dtd).
+
+Sample extracted result file extract.csv is under data/.
+
+You can modify extractor.py and extract by your self.
+
+## License
+
+See [LICENSE](LICENSE)
---
+++ code/extractor.py
@@ -0,0 +1,39 @@
+import sys
+import lxml.etree as ET
+
+xmlfile = sys.argv[1]
+dtdfile = sys.argv[2]
+csvfile = sys.argv[3]
+dtd = ET.DTD(file=dtdfile)
+
+records = list()
+for event, element in ET.iterparse(xmlfile, load_dtd=True):
+  if element.tag == 'article':
+    try:
+      year = int(element.find('year').text)
+      if 2010 < year <= 2018:
+        record = dict()
+        record['year'] = year
+        record['authors'] = [author.text for author in element.findall("author")]
+        record['journal'] = element.find('journal').text
+        records.append(record)
+    except AttributeError:
+      continue
+
+import collections
+authority = collections.defaultdict(int)
+for record in records:
+  for author in record['authors']:
+    authority[author] += 1
+
+import heapq
+authorities = heapq.nlargest(20000, authority, lambda x: authority[x])
+
+out = open(csvfile, "w")
+for record in records:
+  authors = list(filter((lambda x: x in authorities), record['authors']))
+  if len(authors) > 0:
+    out.write(record['journal'])
+    out.write(';')
+    out.write(';'.join(authors))
+    out.write("\n")
