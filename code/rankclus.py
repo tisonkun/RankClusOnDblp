c4bb4146cc61ba54f14cf26b05a2820b8e10b62f
Tison <wander4096@gmail.com>
2018-06-02 16:52:14 +0800

    [Impl] Speed Up Data Load
    1. Change extract.csv format to 'journal;author[;author;...]'
    2. Greatly speed up data load, aviod string::split iteration

--- code/rankclus.py
+++ code/rankclus.py
@@ -1,87 +1,63 @@
 import sys
-import pandas as pd
+import csv
 
 from pprint import pprint
 
-def main():
-    csvfile = sys.argv[1]
-    data = pd.read_csv(csvfile, sep='$', names=['journal', 'authors'])
-
-    """
-    Configure for algorithm.
-
-    @param K     : number of cluster
-    @param MT    : number of main iterate turn
-    @param RT    : number of ranking algorithm iterate turn
-    @param EMT   : number of EM algorithm iterate turn
-    @param alpha : learning rate
-    """
-
-    K = 15
-    MT = 25
-    RT = 10
-    EMT = 5
-    alpha = 0.95
-
-    """
-    Compose network from pandas.DataFrame.
-
-    @value journals       : set of all journals
-    @value authors        : set of all authors
-    @value author2author  : weight of edge, (author, author) -> int
-    @value author2journal : weight of edge, (author, journal) -> int
-    @value journal2author : weight of edge, (journal, author) -> int
-    """
-
-    from collections import defaultdict
-    journals = set(data['journal'])
-    authors = set(";".join(data['authors'].tolist()).split(";"))
-    author2author = { author : defaultdict(int) for author in authors }
-    author2journal = { author : defaultdict(int) for author in authors }
-    journal2author = { journal : defaultdict(int) for journal in journals }
-
-    from itertools import combinations
-    data['authors'] = data['authors'].apply(lambda x: x.split(';'))
-    for _, row in data.iterrows():
-        for author in row['authors']:
-            author2journal[author][row['journal']] += 1
-            journal2author[row['journal']][author] += 1
-        for a, b in combinations(row['authors'], 2):
-            author2author[a][b] += 1
-            author2author[b][a] += 1
-
-    pprint('ack')
-    # pprint(author2author)
-
-    """
-    Randomly initialize clusters.
-
-    @variable clusters : clusters of journals
-    """
-
-    from random import randint
-    clusters = None
-    while True:
-        clusters = [[ ] for _ in range(K)]
-        for journal in journals:
-            clusters[randint(0, K-1)].append(journal)
-        if validate_clusters(clusters): break
-
-    pprint(clusters)
-
 """
-Standalone functions.
+Configure for algorithm.
+
+@param K     : number of cluster
+@param MT    : number of main iterate turn
+@param RT    : number of ranking algorithm iterate turn
+@param EMT   : number of EM algorithm iterate turn
+@param alpha : learning rate
 """
 
-def validate_clusters(clusters):
-    """
-    Prevent cluster from being empty.
+K = 15
+MT = 25
+RT = 10
+EMT = 5
+alpha = 0.95
 
-    @param clusters : clusters to be validated
-    """
-    return all(len(cluster) > 0 for cluster in clusters)
+csvfile = sys.argv[1]
 
 """
-Trigger main function.
+Compose network from pandas.DataFrame.
+
+@value journals       : set of all journals
+@value authors        : set of all authors
+@value author2author  : weight of edge, (author, author) -> int
+@value author2journal : weight of edge, (author, journal) -> int
+@value journal2author : weight of edge, (journal, author) -> int
 """
-main()
+
+journals = set()
+authors = set()
+
+records = csv.reader(open(csvfile), delimiter=";")
+for record in records:
+    journals.add(record[0])
+    for author in record[1:]:
+        authors.add(author)
+
+pprint('ack')
+
+from collections import defaultdict    
+author2author = { author : defaultdict(int) for author in authors }
+author2journal = { author : defaultdict(int) for author in authors }
+journal2author = { journal : defaultdict(int) for journal in journals }
+records = csv.reader(open(csvfile), delimiter=";")
+for record in records:
+    line_journal = record[0]
+    line_authors = record[1:]
+    for i in range(len(line_authors)):
+        journal2author[line_journal][line_authors[i]] += 1
+        author2journal[line_authors[i]][line_journal] += 1
+        for j in range(i+1, len(line_authors)):
+            author2author[line_authors[i]][line_authors[j]] += 1
+            author2author[line_authors[j]][line_authors[i]] += 1
+
+pprint('ack')
+print(journal2author)
+print(author2author)
+print(author2journal)
